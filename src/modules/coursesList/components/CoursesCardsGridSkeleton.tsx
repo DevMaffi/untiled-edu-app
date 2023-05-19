360b6f5d58b32f2b6107f82019f91ffe24db4163
devmaffi <devmaffi550@gmail.com>
2023-05-19 17:47:58 +0300

    refactor: add courses grid skeleton units

--- src/modules/coursesList/components/CoursesCardsGridSkeleton.tsx
+++ src/modules/coursesList/components/CoursesCardsGridSkeleton.tsx
@@ -5,7 +5,7 @@ import { SimpleGrid, Skeleton } from '@chakra-ui/react'
 export default function CoursesCardsGridSkeleton() {
   return (
     <SimpleGrid columns={3} spacing={8}>
-      {_.range(3).map(key => (
+      {_.range(6).map(key => (
         <Skeleton key={key} sx={{ height: '428px', borderRadius: 8 }} />
       ))}
     </SimpleGrid>
