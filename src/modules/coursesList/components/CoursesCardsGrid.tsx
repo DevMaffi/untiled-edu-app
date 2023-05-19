ca8b74b6771abf5d33aca74a84820dd7a393b400
devmaffi <devmaffi550@gmail.com>
2023-05-19 13:54:25 +0300

    refactor: improve alerts composition

--- src/modules/coursesList/components/CoursesCardsGrid.tsx
+++ src/modules/coursesList/components/CoursesCardsGrid.tsx
@@ -24,19 +24,47 @@ export default function CoursesCardsGrid() {
 
   if (error) {
     return (
-      <Alert status={'error'}>
-        <AlertIcon />
-        <AlertTitle>Courses loading error!</AlertTitle>
-        <AlertDescription>{error}</AlertDescription>
+      <Alert
+        status={'error'}
+        sx={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '180px',
+          textAlign: 'center',
+        }}
+        variant={'subtle'}
+      >
+        <AlertIcon boxSize={'40px'} />
+        <AlertTitle sx={{ mt: 4, mb: 2, fontSize: 'lg' }}>
+          Courses loading error!
+        </AlertTitle>
+        <AlertDescription sx={{ maxW: 'xl' }}>{error}</AlertDescription>
       </Alert>
     )
   }
 
   if (!processedCourses.length) {
     return (
-      <Alert status={'info'}>
-        <AlertIcon />
-        <AlertDescription>Жодного курсу не знайдено :{'('}</AlertDescription>
+      <Alert
+        status={'info'}
+        sx={{
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          minHeight: '180px',
+          textAlign: 'center',
+        }}
+        variant={'subtle'}
+      >
+        <AlertIcon boxSize={'40px'} />
+        <AlertTitle sx={{ mt: 4, mb: 2, fontSize: 'lg' }}>
+          Курсів не знайдено!
+        </AlertTitle>
+        <AlertDescription sx={{ maxW: 'lg' }}>
+          Жодного курсу за вказаними параметрами не знайдено. Спробуйте змінити
+          деякі параметри або зменшити складність запиту.
+        </AlertDescription>
       </Alert>
     )
   }
